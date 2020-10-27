d49110b92b30d118285113440e83a079c84d5783
pandaCure <zzyamor@163.com>
2020-10-27 17:00:53 +0800

    完成更新基础镜像操作

--- src/need-build-base-image.js
+++ src/need-build-base-image.js
@@ -1,2 +1,22 @@
-const isNeedBuildBaseImage = () => {}
+const fs = require('fs')
+const semver = require('semver')
+const { cachePackagePath, contextPackagePath } = require('./need-path')
+const createDockerBaseImage = require('./create-docker-image')
+const isNeedBuildBaseImage = () => {
+  const cacheData = JSON.parse(fs.readFileSync(cachePackagePath, 'utf-8'))
+  const { devDependencies, dependencies } = cacheData
+  const package = require(contextPackagePath)
+  const contextDevDependencies = package.devDependencies
+  const contextDependencies = package.dependencies
+  // TODO: 相等且install package is same || 版本号兼容问题
+  if (
+    Object.keys(contextDependencies).length !== Object.keys(dependencies).length ||
+    Object.keys(contextDecontextDevDependenciespendencies).length !== Object.keys(devDependencies).length
+  ) {
+    cacheData.devDependencies = contextDevDependencies
+    cacheData.dependencies = contextDependencies
+    fs.writeFileSync(cachePackagePath, JSON.stringify(cacheData, null, 2))
+    createDockerBaseImage()
+  }
+}
 module.exports = isNeedBuildBaseImage
--- src/need-path.js
+++ src/need-path.js
@@ -2,6 +2,9 @@ const path = require('path')
 const homePath = process.env.HOME || process.env.USERPROFILE
 const cachePackageFile = 'cachePackage.json'
 const cachePackagePath = path.resolve(homePath, cachePackageFile)
+const contextPath = process.cwd()
+const contextPackagePath = path.resolve(contextPath, 'package.json')
 module.exports = {
   cachePackagePath,
+  contextPackagePath,
 }
